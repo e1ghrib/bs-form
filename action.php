338b5e8b59eb9d6a8612b9d4f5c7e7a5ee0a6f0a
E1Ghrib <elghribabdeloumne@gmail.com>
2022-05-07 17:45:41 +0000

    add action.php

---
+++ action.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <td>Fist Name</td>
+            <td><?= $_POST['fname']?></td>
+        </tr>
+        <tr>
+            <td>Last name</td>
+            <td><?= $_POST['lname']?></td>
+        </tr>
+    </table>
+</body>
+</html>
